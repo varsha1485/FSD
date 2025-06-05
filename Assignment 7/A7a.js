30b2d5aa3a1c1fdd5b4dbe859035118b7ffebe5c
varsha1485 <120306864+varsha1485@users.noreply.github.com>
2025-06-05 08:22:27 +0530

    Add files via upload

---
+++ Assignment 7/A7a.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    document.getElementById("message").textContent = "Thanks for clicking! Have a great day.";
+}
---
+++ Assignment 7/A7q2.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "Thanks for clicking! Have a great day!";
+    messageElement.style.color = "#e74c3c";
+}
